d3171b7c311746c91133134a8f55b375633dc825
Emily Chen <emilychen55@gmail.com>
2014-08-27 01:35:51 -0700

    Updating index.html and index.css

--- mobile/violet/platforms/android/ant-build/AndroidManifest.xml.d
+++ mobile/violet/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/mkim-hj/Documents/Nokia/Apollo/mobile/violet/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/mkim-hj/Documents/Nokia/Apollo/mobile/violet/platforms/android/AndroidManifest.xml \
+/Users/emilychen55/Documents/apps/Apollo/mobile/violet/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/emilychen55/Documents/apps/Apollo/mobile/violet/platforms/android/AndroidManifest.xml \
